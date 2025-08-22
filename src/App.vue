23bc59e257001d9ace7a25eae26cf0f244a13197
Wei Yang <your.email@student.monash.edu>
2025-08-22 21:47:53 +1000

    Week 3: Added Bootstrap styling, form submission, card layout, responsive design

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div id="app">
-    <JSON />
+    <!-- <JSON /> -->
+    <LoginForm />
   </div>
 </template>
 
 <script setup>
-import JSON from './components/JSON.vue'
+// import JSON from './components/JSON.vue'
+import LoginForm from './components/LoginForm.vue'
 </script>
 
 <style>
-
+/* Global styles can be added here */
 </style>
